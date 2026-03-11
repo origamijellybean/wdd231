d56c8800fca49de9026b829393f09f807f5fbe27
origamijellybean <thunter@byupathway.edu>
2026-03-10 22:32:57 -0600

    home assignment week 1

---
+++ scripts/navigation.js
@@ -0,0 +1,8 @@
+const navButton = document.querySelector("#nav-button");
+const navBar = document.querySelector("#nav-bar");
+const body = document.querySelector("body");
+navButton.addEventListener("click", () => {
+    navButton.classList.toggle("show");
+    navBar.classList.toggle("show");
+    body.classList.toggle("hide");
+});
