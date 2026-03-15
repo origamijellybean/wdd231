3240879c0f1f5787a18e931f9e21b3aeb95430b0
origamijellybean <thunter@byupathway.edu>
2026-03-15 01:08:01 -0600

    css edits

--- chamber/scripts/contacts.js
+++ chamber/scripts/contacts.js
@@ -9,30 +9,42 @@ async function getData() {
 }
 
 function displayData(companies) {
-    companies.forEach((company) => {
-        const card = document.createElement("section");
-        const name = document.createElement("h2");
-        const image = document.createElement("img");
-        const phone = document.createElement("p");
-        const companyurl = document.createElement("p");
-        const member_level = document.createElement("p");
-        name.innerHTML = `${company.name}`;
-        image.setAttribute("src", company.image);
-        image.setAttribute("alt", `Image for ${company.name}.`);
-        image.setAttribute("loading", "lazy");
-        image.setAttribute("width", "340");
-        image.setAttribute("height", "340");
-        image.classList.add("companyImage");
-        phone.innerHTML = `<strong>Phone Number:</strong> ${company.phone}`;
-        companyurl.innerHTML = `<strong>Company Website:</strong> ${company.companyurl}`;
-        member_level.innerHTML = `<strong>Membership Level:</strong> ${company.member_level}`;
-        card.appendChild(name);
-        card.appendChild(phone);
-        card.appendChild(companyurl);
-        card.appendChild(member_level);
-        card.appendChild(image);
-        cards.appendChild(card);
+    let companieshtml = "";
+    companies.forEach(company => {
+        companieshtml += `<section>
+            <h2>${company.name}</h2>
+            <p><strong>Phone Number:</strong> ${company.phone}</p>
+            <p><strong>Company Website:</strong> ${company.companyurl}</p>
+            <p><strong>Membership Level:</strong> ${company.member_level}</p>
+            <img src="${company.image}" alt="Image for ${company.name}." loading="lazy" width="200" height="200" class="companyImage">
+        </section>`;
     });
+    // companies.forEach((company) => {
+
+    //     const card = document.createElement("section");
+    //     const name = document.createElement("h2");
+    //     const image = document.createElement("img");
+    //     const phone = document.createElement("p");
+    //     const companyurl = document.createElement("p");
+    //     const member_level = document.createElement("p");
+    //     name.innerHTML = `${company.name}`;
+    //     image.setAttribute("src", company.image);
+    //     image.setAttribute("alt", `Image for ${company.name}.`);
+    //     image.setAttribute("loading", "lazy");
+    //     image.setAttribute("width", "200");
+    //     image.setAttribute("height", "200");
+    //     image.classList.add("companyImage");
+    //     phone.innerHTML = `<strong>Phone Number:</strong> ${company.phone}`;
+    //     companyurl.innerHTML = `<strong>Company Website:</strong> ${company.companyurl}`;
+    //     member_level.innerHTML = `<strong>Membership Level:</strong> ${company.member_level}`;
+    //     card.appendChild(name);
+    //     card.appendChild(phone);
+    //     card.appendChild(companyurl);
+    //     card.appendChild(member_level);
+    //     card.appendChild(image);
+    //     cards.appendChild(card);
+    // });
+    cards.innerHTML = companieshtml;
 }
 
 getData();
