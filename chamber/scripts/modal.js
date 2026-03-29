9fe16274d9e38b488ab48951fde6d83549e583a8
origamijellybean <thunter@byupathway.edu>
2026-03-29 00:34:02 -0600

    fieldset styled

--- chamber/scripts/modal.js
+++ chamber/scripts/modal.js
@@ -3,7 +3,7 @@ const dialogs = document.querySelectorAll("#membership-levels > dialog");
 
 sections.forEach((section, index) => {
     const dialog = dialogs[index];
-    const openDialog = section.querySelector("a");
+    const openDialog = section.querySelector("button");
     const closeDialog = dialog.querySelector(".closeDialog");
     openDialog.addEventListener("click", () => {
         dialog.showModal();
