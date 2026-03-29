df0a259d94c4bc22e7263e86d2bfe309a958f81e
origamijellybean <thunter@byupathway.edu>
2026-03-28 21:34:31 -0600

    Added time stamp addition

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -3,3 +3,9 @@ const lastModified = document.getElementById("lastModified");
 const today = new Date();
 currentyear.innerHTML = `© <span>${today.getFullYear()} Tyson Hunter, Utah</span>`;
 lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
+
+const timestamp = document.getElementById("timestamp");
+if (timestamp) {
+    const time = new Date();
+    timestamp.value = time.toLocaleString();
+}
--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -2,11 +2,13 @@ const infoString = window.location.search;
 const myInfo = new URLSearchParams(window.location.search);
 results = document.querySelector("#results");
 results.innerHTML = `
-<p>Your Information:</p>
+<h2>Your Information:</h2>
 <p>Name: ${myInfo.get("first")} ${myInfo.get("last")}</p>
-<p>Organization Information: ${myInfo.get("title")} of ${myInfo.get("date")} of the ${myInfo.get("organization")}.</p>
-<p>Organization Description: ${myInfo.get("description")}</p>
+<p>Title: ${myInfo.get("title")}
+<p>Organization: ${myInfo.get("organization")}</p>
+<p>Description: ${myInfo.get("description")}</p>
 <p>Your phone: ${ myInfo.get("phone") }</p>
 <p>Your email: ${myInfo.get("email")}</p>
-<p>Memebership Level: ${myInfo.get("membership")}</p>
+<p>Membership Level: ${myInfo.get("membership")}</p>
+<p>Time: ${myInfo.get("timestamp")}</p>
 `;
