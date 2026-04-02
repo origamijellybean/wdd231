8ed24e6115c6486fab8f21432abd472660421a1e
origamijellybean <thunter@byupathway.edu>
2026-04-02 00:22:32 -0600

    style and addition of main final page and siteplan

---
+++ final/scripts/date.js
@@ -0,0 +1,5 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.getElementById("lastModified");
+const today = new Date();
+currentyear.innerHTML = `© <span>${today.getFullYear()} Tyson Hunter, Utah</span>`;
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
