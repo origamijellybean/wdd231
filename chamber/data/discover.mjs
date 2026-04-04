97ee5b2de41ee9b4fa8506a161ad4302dea95a3a
origamijellybean <thunter@byupathway.edu>
2026-04-03 22:34:17 -0600

    finished adding the js for the places

--- chamber/data/discover.mjs
+++ chamber/data/discover.mjs
@@ -1,52 +1,50 @@
-{
-    "places"[
+export const places = [
         {
-            "name":"Willard Bay State Park",
-            "address":"900 W 650 N, Willard, UT 84340",
-            "image":"../images/willard-canyon.webp",
+            "name": "Willard Bay State Park",
+            "address": "900 W 650 N, Willard, UT 84340",
+            "image": "./images/willard-canyon.webp",
             "description": "Willard Bay, a fresh-water subportion of the Great Salt Lake, is a camping, fishing, and boating park. Day or seasons passes required."
         },
         {
-            "name":"Willard Canyon",
-            "address":"Haul Rd & E 300 N, Willard, UT 84340",
-            "image":"../images/willard-canyon.webp",
-            "description":"This canyon is a beautiful hike with a small area for camping and waterfalls, used for over 100 years."
+            "name": "Willard Canyon",
+            "address": "Haul Rd & E 300 N, Willard, UT 84340",
+            "image": "./images/willard-canyon.webp",
+            "description": "This canyon is a beautiful hike with a small area for camping and waterfalls, used for over 100 years."
         },
         {
-            "name":"Maddox",
-            "address":"1900 S Hwy 89, Perry, UT 84302",
-            "image":"../images/maddox-chewandchat.webp",
-            "description":"A local staple, Maddox is a family friendly fancy restaurant, with kids menu and delicious dishes including turkey steaks, fish, bison, and delicious endless rolls!"
+            "name": "Maddox",
+            "address": "1900 S Hwy 89, Perry, UT 84302",
+            "image": "./images/maddox-chewandchat.webp",
+            "description": "A local staple, Maddox is a family friendly fancy restaurant, with kids menu and delicious dishes including turkey steaks, fish, bison, and delicious endless rolls!"
         },
         {
-            "name":"Apple Creek",
-            "address":"875 North Main Street, S Hwy 89, Willard, UT 84340",
-            "image":"../images/apple-creek.webp",
-            "description":"A local amish store that sells furniture, icecream, cheese, candies, and bulk items like flour and spices."
+            "name": "Apple Creek",
+            "address": "875 North Main Street, S Hwy 89, Willard, UT 84340",
+            "image": "./images/apple-creek.webp",
+            "description": "A local amish store that sells furniture, icecream, cheese, candies, and bulk items like flour and spices."
         },
         {
-            "name":"Millstream Classic Car Museum",
-            "address":"255 E 1080 N, Willard, UT 84340",
-            "image":"../images/museum.webp",
-            "description":"Locally owned classic car museum. Quite a collection!"
+            "name": "Millstream Classic Car Museum",
+            "address": "255 E 1080 N, Willard, UT 84340",
+            "image": "./images/museum.webp",
+            "description": "Locally owned classic car museum. Quite a collection!"
         },
         {
-            "name":"Willow Creek Park",
-            "address":"57 N 200 E, Willard, UT, 84340",
-            "image":"",
-            "description":"A park with baseball fields, trees, and a playground."
+            "name": "Willard Park",
+            "address": "135 N 100 E, Willard, UT 84340",
+            "image": "./images/dyke.webp",
+            "description": "A retention basin for creek overflow that has also been turned into a park with trails, playground, and bowery!"
         },
         {
-            "name":"Fruit Stands",
-            "address":"S Hwy 89",
-            "image":"../images/fruit-stand.webp",
-            "description":"Highway 89 is known as the fruitway. Local produce at Taggies and other local farms and orchards are a must stop!"
+            "name": "Fruit Stands",
+            "address": "S Hwy 89",
+            "image": "./images/fruit-stand.webp",
+            "description": "Highway 89 is known as the fruitway. Local produce at Taggies and other local farms and orchards are a must stop!"
         },
         {
-            "name":"Willard Mountains",
-            "address":"FR 20084, UT",
-            "image":"../images/willard-mountains-discover.webp",
-            "description":"Hungry for wilderness? Hike, bike, 4-wheel, or drive up to experience the majestic Willard Mountains, accessible through Mantua in the late summer/fall."
+            "name": "Willard Mountains",
+            "address": "FR 20084, UT",
+            "image": "./images/willard-mountains-discover.webp",
+            "description": "Hungry for wilderness? Hike, bike, 4-wheel, or drive up to experience the majestic Willard Mountains, accessible through Mantua in the late summer/fall."
         }
     ]
-}
---
+++ chamber/scripts/places.js
@@ -0,0 +1,27 @@
+import { places } from "../data/discover.mjs";
+console.log(places);
+
+
+function displayPlaces() {
+    places.forEach((place) => {
+        const card = document.createElement("section");
+        const name = document.createElement("h2");
+        const photo = document.createElement("img");
+        const address = document.createElement("p");
+        const description = document.createElement("p");
+        name.innerHTML = `${place.name}`;
+        photo.setAttribute("src", place.image);
+        photo.setAttribute("alt", `Picture of ${place.name}`);
+        photo.setAttribute("loading", "lazy");
+        photo.setAttribute("width", "300");
+        photo.setAttribute("height", "200");
+        address.innerHTML = `Address: ${place.address}`;
+        description.innerHTML = `Description: ${place.description}`;
+        card.appendChild(name);
+        card.appendChild(photo);
+        card.appendChild(address);
+        card.appendChild(description);
+        discover.appendChild(card);
+    });
+}
+displayPlaces()
