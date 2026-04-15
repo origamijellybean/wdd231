a01e123fb32c716497b6035cb9fb56a713a6e184
origamijellybean <thunter@byupathway.edu>
2026-04-14 21:22:06 -0600

    some changes and additions

---
+++ final/scripts/form.js
@@ -0,0 +1,8 @@
+const myInfo = new URLSearchParams(window.location.search);
+results = document.querySelector("#results");
+results.innerHTML = `
+<h2>Your Info</h2>
+<p>Name: ${myInfo.get("first")} ${myInfo.get("last")}</p>
+<p>Your phone: ${myInfo.get("phone")}</p>
+<p>Your email: ${myInfo.get("email")}</p>
+`;
